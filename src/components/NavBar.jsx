51eb35481829c09a60a65312b0435bc0727f25e7
isaac gonzalez <isa.gonzalezl@duocuc.cl>
2026-03-08 15:02:23 -0300

    logo github agregado a nav

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,6 +4,7 @@ import logo from '../assets/img/logo.svg';
 import navIcon1 from '../assets/img/nav-icon1.svg';
 import navIcon2 from '../assets/img/nav-icon2.svg';
 import navIcon3 from '../assets/img/nav-icon3.svg';
+import github from '../assets/img/github.svg';
 import { HashLink } from 'react-router-hash-link';
 import {
   BrowserRouter as Router
@@ -51,7 +52,7 @@ export const NavBar = () => {
             <span className="navbar-text">
               <div className="social-icon">
                 <a href="https://www.linkedin.com/in/isaac-gonz%C3%A1lez-564b83236/"><img src={navIcon1} alt="Linkedin" /></a>
-                <a href="#"><img src={navIcon2} alt="" /></a>
+                <a href="https://github.com/isaacgonza-ux"><img src={github} alt="Github" /></a>" 
                 <a href="https://www.instagram.com/isa.bn_g/"><img src={navIcon3} alt="Instagram" /></a>
               </div>
               <HashLink to='#connect'>
